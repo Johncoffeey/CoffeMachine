d362b0e3e61af693aa9060a2bef02df850054299
Surftown1 <31923273+Surftown1@users.noreply.github.com>
2017-10-24 11:30:28 +0200

    idk

--- src/cofefemachine/CofefeMachine.java
+++ src/cofefemachine/CofefeMachine.java
@@ -5,12 +5,11 @@
  */
 package cofefemachine;
 
+import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -20,9 +19,14 @@ import javafx.stage.Stage;
 public class CofefeMachine extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/FXML.fxml"));
         
+        Scene scene = new Scene(root);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
         
     }
 
